5778035a0cc021a0158225bbd715ac39ba9091be
huanghan <2235812260@qq.com>
2022-05-14 13:38:06 +0800

    修改部分布局

--- app/src/main/java/com/example/stockup/MainActivity.java
+++ app/src/main/java/com/example/stockup/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.stockup;
 
 import androidx.appcompat.app.AppCompatActivity;
+import android.widget.Button;
+import android.view.View;
 
 import android.os.Bundle;
+import android.widget.Toast;
+import com.roughike.bottombar.BottomBar;
+import com.roughike.bottombar.OnTabSelectListener;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
